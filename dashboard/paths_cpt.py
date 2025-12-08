fb87ec04777afc6a9a9a27cb18aa035b54ca9a6a
m-mburu <mmburu20@gmail.com>
2025-12-08 13:28:55 +0300

    Track .pkl files and update .gitignore

--- dashboard/paths_cpt.py
+++
@@ -1,3 +0,0 @@
-PATH_TO_PARQUET = "C:/Users/volte/OneDrive/Dokumente/usecase_cpt/raw_parquet.parquet"
-
-PATH_TO_MODEL = "C:/Users/volte/Desktop/CPT_uni_proj/modeling/rf_model_export.pkl"
--- modeling/paths_cpt.py
+++
@@ -1,4 +0,0 @@
-
-PATH_TO_PARQUET = "C:/Users/volte/OneDrive/Dokumente/usecase_cpt/raw_parquet.parquet"
-
-PATH_TO_MODEL = "C:/Users/volte/OneDrive/Dokumente/usecase_cpt/CPT_usecase/modeling/rf_model_export.pkl"
